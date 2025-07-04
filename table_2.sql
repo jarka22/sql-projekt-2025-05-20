39abc4316dea1ecf96890d11bff6ff7f82d0f2fa
Jarka <jasus@seznam.cz>
2025-07-04 22:00:04 +0200

    Add changes

--- table_2.sql
+++ table_2.sql
@@ -1,6 +1,16 @@
-SELECT *
+CREATE TABLE t_jarmila_sustkova_project_sql_secondary_final AS
+SELECT 
+	c.*,
+	e.country AS economies_country,
+	e.YEAR,
+	e.gdp,
+	e.population AS economies_population,
+	e.gini,
+	e.taxes,
+	e.fertility,
+	e.mortaliy_under5
 FROM economies e
 LEFT JOIN countries c
 	ON c.country = e.country
-	WHERE e.year >= 2006 AND e.year <= 2018
+WHERE e.year >= 2006 AND e.year <= 2018 AND c.country=e.country
 ORDER BY c.country, e.YEAR;
